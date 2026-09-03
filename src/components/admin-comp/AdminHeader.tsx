279b4e7846aec0889a275f631d9064d69c80bad5
Ritesh Sinha <ritesh.sinha.737001@gmail.com>
2026-09-03 20:56:04 +0530

    vercel deploy

--- src/components/admin-comp/AdminHeader.tsx
+++ src/components/admin-comp/AdminHeader.tsx
@@ -33,57 +33,30 @@ export default function AdminHeader({ children }: AdminHeaderProps) {
         backgroundColor: background,
       }}
     >
-      {/* ================================================ */}
       {/* SIDEBAR */}
-      {/* ================================================ */}
-
       <AdminSidebar />
 
-      {/* ================================================ */}
       {/* RIGHT SIDE */}
-      {/* ================================================ */}
-
       <Box
         sx={{
-          marginLeft: {
-            xs: "200px",
-            md: "240px",
-          },
-
-          width: {
-            xs: "calc(100% - 200px)",
-            md: "calc(100% - 240px)",
-          },
-
+          marginLeft: { xs: "200px", md: "240px" },
+          width: { xs: "calc(100% - 200px)", md: "calc(100% - 240px)" },
           minHeight: "100vh",
           backgroundColor: background,
           boxSizing: "border-box",
         }}
       >
-        {/* ================================================ */}
         {/* HEADER */}
-        {/* ================================================ */}
-
         <Box
           sx={{
-            height: {
-              xs: "60px",
-              md: "70px",
-            },
-
+            height: { xs: "60px", md: "70px" },
             width: "100%",
             backgroundColor: background,
             borderBottom: `1px solid ${borderColor}`,
-
             display: "flex",
             alignItems: "center",
             justifyContent: "space-between",
-
-            px: {
-              xs: "12px",
-              md: "16px",
-            },
-
+            px: { xs: "12px", md: "16px" },
             boxSizing: "border-box",
             flexShrink: 0,
           }}
@@ -94,12 +67,7 @@ export default function AdminHeader({ children }: AdminHeaderProps) {
             <Typography
               sx={{
                 color: txtWhite,
-
-                fontSize: {
-                  xs: "12px",
-                  md: "16px",
-                },
-
+                fontSize: { xs: "12px", md: "16px" },
                 fontWeight: 700,
               }}
             >
@@ -109,12 +77,7 @@ export default function AdminHeader({ children }: AdminHeaderProps) {
             <Typography
               sx={{
                 color: txtMuted,
-
-                fontSize: {
-                  xs: "8px",
-                  md: "10px",
-                },
-
+                fontSize: { xs: "8px", md: "10px" },
                 mt: "3px",
               }}
             >
@@ -122,47 +85,27 @@ export default function AdminHeader({ children }: AdminHeaderProps) {
             </Typography>
           </Box>
 
-          {/* ================================================ */}
           {/* RIGHT */}
-          {/* ================================================ */}
-
           <Box
             sx={{
               display: "flex",
               alignItems: "center",
-
-              gap: {
-                xs: "10px",
-                md: "12px",
-              },
+              gap: { xs: "10px", md: "12px" },
             }}
           >
             {/* SEARCH */}
 
             <Box
               sx={{
-                display: {
-                  xs: "none",
-                  md: "flex",
-                },
-
-                width: {
-                  md: "200px",
-                },
-
-                height: {
-                  md: "35px",
-                },
-
+                display: { xs: "none", md: "flex" },
+                width: { xs: "0px", md: "200px" },
+                height: { xs: "0px", md: "35px" },
                 border: `1px solid ${borderColor}`,
                 borderRadius: "6px",
                 backgroundColor: inputBackground,
-
                 alignItems: "center",
-
                 px: "16px",
                 gap: "10px",
-
                 boxSizing: "border-box",
               }}
             >
@@ -192,27 +135,12 @@ export default function AdminHeader({ children }: AdminHeaderProps) {
               />
             </Box>
 
-            {/* ================================================ */}
             {/* NOTIFICATION */}
-            {/* ================================================ */}
-
-            <Box
-              sx={{
-                position: "relative",
-              }}
-            >
+            <Box sx={{ position: "relative" }}>
               <IconButton
                 sx={{
-                  width: {
-                    xs: "25px",
-                    md: "35px",
-                  },
-
-                  height: {
-                    xs: "25px",
-                    md: "35px",
-                  },
-
+                  width: { xs: "25px", md: "35px" },
+                  height: { xs: "25px", md: "35px" },
                   border: `1px solid ${borderColor}`,
                   borderRadius: "6px",
                   color: txtWhite,
@@ -221,11 +149,7 @@ export default function AdminHeader({ children }: AdminHeaderProps) {
                 <NotificationsNoneOutlined
                   sx={{
                     color: txtWhite,
-
-                    fontSize: {
-                      xs: "12px",
-                      md: "16px",
-                    },
+                    fontSize: { xs: "12px", md: "16px" },
                   }}
                 />
               </IconButton>
@@ -235,40 +159,17 @@ export default function AdminHeader({ children }: AdminHeaderProps) {
               <Box
                 sx={{
                   position: "absolute",
-
-                  top: {
-                    xs: "-3px",
-                    md: "-7px",
-                  },
-
-                  right: {
-                    xs: "-4px",
-                    md: "-7px",
-                  },
-
-                  width: {
-                    xs: "12px",
-                    md: "16px",
-                  },
-
-                  height: {
-                    xs: "12px",
-                    md: "16px",
-                  },
-
+                  top: { xs: "-3px", md: "-7px" },
+                  right: { xs: "-4px", md: "-7px" },
+                  width: { xs: "12px", md: "16px" },
+                  height: { xs: "12px", md: "16px" },
                   borderRadius: "50%",
                   backgroundColor: red,
                   color: txtWhite,
-
                   display: "flex",
                   alignItems: "center",
                   justifyContent: "center",
-
-                  fontSize: {
-                    xs: "8px",
-                    md: "10px",
-                  },
-
+                  fontSize: { xs: "8px", md: "10px" },
                   fontWeight: 700,
                 }}
               >
@@ -276,29 +177,19 @@ export default function AdminHeader({ children }: AdminHeaderProps) {
               </Box>
             </Box>
 
-            {/* ================================================ */}
             {/* VIEW WEBSITE */}
-            {/* ================================================ */}
-
             <Box
               onClick={() => {
                 window.location.href = "/";
               }}
               sx={{
-                display: {
-                  xs: "none",
-                  md: "flex",
-                },
-
+                display: { xs: "none", md: "flex" },
                 height: "35px",
                 px: "10px",
-
                 border: `1px solid ${Primary}`,
                 borderRadius: "6px",
-
                 alignItems: "center",
                 gap: "4px",
-
                 cursor: "pointer",
               }}
             >
@@ -316,47 +207,27 @@ export default function AdminHeader({ children }: AdminHeaderProps) {
               <OpenInNewOutlined
                 sx={{
                   color: txtWhite,
-
-                  fontSize: {
-                    xs: "12px",
-                    md: "16px",
-                  },
+                  fontSize: { xs: "12px", md: "16px" },
                 }}
               />
             </Box>
 
-            {/* ================================================ */}
             {/* ADMIN */}
-            {/* ================================================ */}
-
             <Box
               sx={{
                 display: "flex",
                 alignItems: "center",
-
-                gap: {
-                  xs: "4px",
-                  md: "8px",
-                },
+                gap: { xs: "4px", md: "8px" },
               }}
             >
               {/* ADMIN ICON */}
 
               <Box
                 sx={{
-                  width: {
-                    xs: "25px",
-                    md: "30px",
-                  },
-
-                  height: {
-                    xs: "25px",
-                    md: "30px",
-                  },
-
+                  width: { xs: "25px", md: "30px" },
+                  height: { xs: "25px", md: "30px" },
                   borderRadius: "50%",
                   border: `1px solid ${borderColor}`,
-
                   display: "flex",
                   alignItems: "center",
                   justifyContent: "center",
@@ -365,27 +236,17 @@ export default function AdminHeader({ children }: AdminHeaderProps) {
                 <PersonOutlined
                   sx={{
                     color: txtWhite,
-
-                    fontSize: {
-                      xs: "12px",
-                      md: "16px",
-                    },
+                    fontSize: { xs: "12px", md: "16px" },
                   }}
                 />
               </Box>
 
               {/* ADMIN INFO */}
-
               <Box>
                 <Typography
                   sx={{
                     color: txtWhite,
-
-                    fontSize: {
-                      xs: "8px",
-                      md: "10px",
-                    },
-
+                    fontSize: { xs: "8px", md: "10px" },
                     fontWeight: 600,
                   }}
                 >
@@ -395,11 +256,7 @@ export default function AdminHeader({ children }: AdminHeaderProps) {
                 <Typography
                   sx={{
                     color: txtMuted,
-
-                    fontSize: {
-                      xs: "8px",
-                      md: "10px",
-                    },
+                    fontSize: { xs: "8px", md: "10px" },
                   }}
                 >
                   Super Admin
@@ -409,29 +266,19 @@ export default function AdminHeader({ children }: AdminHeaderProps) {
               <KeyboardArrowDown
                 sx={{
                   color: txtLight,
-
-                  fontSize: {
-                    xs: "10px",
-                    md: "12px",
-                  },
+                  fontSize: { xs: "10px", md: "12px" },
                 }}
               />
             </Box>
           </Box>
         </Box>
 
-        {/* ================================================ */}
         {/* PAGE CONTENT */}
-        {/* ================================================ */}
-
         <Box
           component="main"
           sx={{
             width: "100%",
-            minHeight: {
-              xs: "calc(100vh - 60px)",
-              md: "calc(100vh - 70px)",
-            },
+            minHeight: { xs: "calc(100vh - 60px)", md: "calc(100vh - 70px)" },
             boxSizing: "border-box",
           }}
         >
