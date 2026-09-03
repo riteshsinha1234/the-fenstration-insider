b0082cffa95a5e97c6e613531162af234b4bc04b
Ritesh Sinha <ritesh.sinha.737001@gmail.com>
2026-09-03 12:24:18 +0530

    configure vercel deployment

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,11 +1,13 @@
 const { initializeApp, cert, getApps } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 
-const serviceAccount = require("../serviceAccountKey.json");
-
 if (!getApps().length) {
   initializeApp({
-    credential: cert(serviceAccount),
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
   });
 }
 
